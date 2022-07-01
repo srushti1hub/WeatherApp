531a006a5c6446804dc4f5894e74dea94321a8ce
Srushti Rathva <76732482+srushti1hub@users.noreply.github.com>
2022-07-01 13:34:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
+// Command Line Application
 const request = require('request')
 const argv  = require('yargs')
 const yargs = require('yargs')
 const chalk = require('chalk')
 
-const url = 'http://api.weatherstack.com/current?access_key=your_api_key&query='
+const url = 'http://api.weatherstack.com/current?access_key=a125d43aef2fb00cac6ffb80b418fed8&query='
 
 const WeatherInfo = (fullurl) => {
-    request({url: fullurl,json: true},(error,response) => 
+    request({url: fullurl,json: true},(Error,response) => 
     {
-        console.log(chalk.magenta(response.body.location.name+','+response.body.location.country+'  '+response.body.location.localtime));
-        console.log(chalk.cyan('Latitude    : '+response.body.location.lat));
-        console.log(chalk.cyan('Longitude   : '+response.body.location.lon));
-        console.log(chalk.yellow('Temperature : '+response.body.current.temperature+' C'));
-        console.log(chalk.green('Wind Speed  : '+response.body.current.wind_speed+' km/hr'));
-        console.log(chalk.blue('Humidity    : '+response.body.current.humidity));
-        console.log(chalk.red('Description : '+response.body.current.weather_descriptions));
+        if(Error)
+        {
+            console.log(chalk.redBright.bold('Unable to connect with weather service :('));
+        }
+        else if(response.body.error)
+        {
+            console.log(chalk.redBright.bold(response.body.error.code+', '+response.body.error.type));
+        }
+        else {
+            console.log(chalk.white.bold.inverse('**WEATHER REPORT**'))
+            console.log(chalk.magenta(response.body.location.name+','+response.body.location.country+'  '+response.body.location.localtime));
+            console.log(chalk.cyan('Latitude    : '+response.body.location.lat));
+            console.log(chalk.cyan('Longitude   : '+response.body.location.lon));
+            console.log(chalk.yellow('Temperature : '+response.body.current.temperature+' C'));
+            console.log(chalk.green('Wind Speed  : '+response.body.current.wind_speed+' km/hr'));
+            console.log(chalk.blue('Humidity    : '+response.body.current.humidity));
+            console.log(chalk.red('Description : '+response.body.current.weather_descriptions));
+        }
     })
 } 
 
@@ -29,7 +41,6 @@ yargs.command({
         }
     },
     handler(argv){
-        console.log(chalk.white.bold.inverse('**WEATHER REPORT**'))
         WeatherInfo(url+argv.city);
     }
 })
